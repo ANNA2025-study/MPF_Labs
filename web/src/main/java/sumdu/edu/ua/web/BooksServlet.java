04e37b8c2e04726936f8d9c7d7ea072f7f03c049
Vlad Piatachenko <vlpiatachenko@gmail.com>
2025-09-27 17:25:04 +0300

    lab 2 but archtest failed

---
+++ web/src/main/java/sumdu/edu/ua/web/BooksServlet.java
@@ -0,0 +1,32 @@
+package sumdu.edu.ua.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import sumdu.edu.ua.persistence.jdbc.JdbcBookRepository;
+import sumdu.edu.ua.core.domain.Book;
+import sumdu.edu.ua.core.domain.PageRequest;
+
+import java.io.IOException;
+import java.util.List;
+
+public class BooksServlet extends HttpServlet {
+
+    private final JdbcBookRepository bookRepo = new JdbcBookRepository();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        try {
+            PageRequest pageRequest = new PageRequest(0, 20);
+
+            List<Book> books = bookRepo.search(null, pageRequest).getItems();
+            req.setAttribute("books", books);
+
+            req.getRequestDispatcher("/WEB-INF/views/books.jsp").forward(req, resp);
+
+        } catch (Exception e) {
+            throw new ServletException("Cannot load books", e);
+        }
+    }
+}
