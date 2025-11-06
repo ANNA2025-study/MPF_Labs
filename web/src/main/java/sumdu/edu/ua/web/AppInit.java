2e1a24c75995efd95f218da507f14ca3777a53c0
Vlad Piatachenko <vlpiatachenko@gmail.com>
2025-11-07 00:54:29 +0200

    Fixed DB init for MVC. Added Demo to add books. Fixed dependencies

--- web/src/main/java/sumdu/edu/ua/web/AppInit.java
+++ web/src/main/java/sumdu/edu/ua/web/AppInit.java
@@ -9,6 +9,12 @@ public class AppInit {
     public static void main(String[] args) {
         SpringApplication.run(AppInit.class, args);
         System.out.println("Started at http://localhost:8080/books");
+        try {
+            sumdu.edu.ua.persistence.jdbc.DbInit.init();
+            System.out.println("DB schema ensured (books table created if absent)");
+        } catch (Exception e) {
+            System.err.println("DB init failed: " + e.getMessage());
+        }
     }
 }
 
--- web/src/main/java/sumdu/edu/ua/web/WebConfig.java
+++ web/src/main/java/sumdu/edu/ua/web/WebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
+
         registry.jsp("/WEB-INF/views/", ".jsp");
     }
 }
