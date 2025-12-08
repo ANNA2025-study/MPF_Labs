a30ab368c394c922b71fcf1676de520e8e8bbf57
Vlad Piatachenko <vlpiatachenko@gmail.com>
2025-12-08 23:16:48 +0200

    sql fix  psql: id bigint

--- persistence/src/main/resources/db/migration/V2__update.sql
+++ persistence/src/main/resources/db/migration/V2__update.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     id integer generated always as identity primary key,
+                                     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                                      username VARCHAR(64) NOT NULL UNIQUE,
     display_name VARCHAR(255)
     );
